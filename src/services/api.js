3b32f7bd4b22601659bdf549a4e12ca68e554aeb
Sergio Lenza <slenzamori.dev@gmail.com>
2022-05-12 11:51:11 +0200

    fix: add some eslint ignores in api.js

--- src/services/api.js
+++ src/services/api.js
@@ -3,9 +3,12 @@ const PRUEBA_KO = 'pruebaKO123';
 const RESPONSE_OK = { status: 200 };
 const RESPONSE_KO = { status: 401 };
 
+// eslint-disable-next-line no-unused-vars
 const submitForm = (pass, repass, optionalQuestion) =>
   new Promise((resolve, reject) =>
+    // eslint-disable-next-line no-promise-executor-return
     setTimeout(() => (pass !== PRUEBA_KO ? resolve(RESPONSE_OK) : reject(RESPONSE_KO)), 3000)
   );
 
+// eslint-disable-next-line import/prefer-default-export
 export { submitForm };
