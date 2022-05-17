da992453d076b8da1fbb1583364ca8642834d5d7
Sergio Lenza <slenzamori.dev@gmail.com>
2022-05-17 17:46:48 +0200

    feat: add footer with links in the final step

--- src/views/PasswordManagerFeedback/PasswordManagerFeedback.js
+++ src/views/PasswordManagerFeedback/PasswordManagerFeedback.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useLocation, Navigate } from 'react-router-dom';
+import { useLocation, Navigate, Link } from 'react-router-dom';
 import { t } from 'i18next';
 import { BsCheck2Circle } from 'react-icons/bs';
 import { MdOutlineWarningAmber } from 'react-icons/md';
@@ -15,21 +15,30 @@ const PasswordManagerFeedback = () => {
   const { error } = state;
 
   return (
-    <article className="wizard--content">
-      {error ? (
-        <Placeholder
-          icon={<MdOutlineWarningAmber size="3em" />}
-          primaryText={t('pwFeedback.thereIsAnError')}
-          secondaryText={t('pwFeedback.weCannotUpdate')}
-        />
-      ) : (
-        <Placeholder
-          icon={<BsCheck2Circle size="3em" />}
-          primaryText={t('pwFeedback.passwordManagerCreated')}
-          secondaryText={t('pwFeedback.loremIpsum')}
-        />
-      )}
-    </article>
+    <>
+      <article className="wizard--content">
+        {error ? (
+          <Placeholder
+            icon={<MdOutlineWarningAmber size="3em" />}
+            primaryText={t('pwFeedback.thereIsAnError')}
+            secondaryText={t('pwFeedback.weCannotUpdate')}
+          />
+        ) : (
+          <Placeholder
+            icon={<BsCheck2Circle size="3em" />}
+            primaryText={t('pwFeedback.passwordManagerCreated')}
+            secondaryText={t('pwFeedback.loremIpsum')}
+          />
+        )}
+      </article>
+      <footer className="wizard--footer end">
+        {error ? (
+          <Link to="/">{t('pwFeedback.backToPasswordManager')}</Link>
+        ) : (
+          <Link to="/to-be-defined">{t('pwFeedback.access')}</Link>
+        )}
+      </footer>
+    </>
   );
 };
 
