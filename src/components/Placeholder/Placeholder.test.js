9c5b4d3aaf0d581a2854b21b0ba9573bc78b411b
Sergio Lenza <slenzamori.dev@gmail.com>
2022-05-18 11:02:32 +0200

    refactor: change verb in test suite of Placeholder

--- src/components/Placeholder/Placeholder.test.js
+++ src/components/Placeholder/Placeholder.test.js
@@ -5,12 +5,12 @@ const renderPlaceholder = () =>
   render(<Placeholder icon="test" primaryText="primary text" secondaryText="secondary text" />);
 
 describe('Placeholder component', () => {
-  it('does render the Placeholder', async () => {
+  it('should render the Placeholder', async () => {
     renderPlaceholder();
     const placeholder = await screen.findByText('test');
     expect(placeholder).toBeInTheDocument();
   });
-  it('does render all the props', async () => {
+  it('should render all the props', async () => {
     renderPlaceholder();
     const icon = await screen.findByText('test');
     expect(icon).toBeInTheDocument();
