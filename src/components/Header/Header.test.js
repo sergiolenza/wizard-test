5f643b7aadfc5450c8d8b5a02fc52b4c3daa0e1b
Sergio Lenza <slenzamori.dev@gmail.com>
2022-05-18 10:48:56 +0200

    refactor: change verb in test suite of Header

--- src/components/Header/Header.test.js
+++ src/components/Header/Header.test.js
@@ -2,9 +2,10 @@ import { render, screen } from '@testing-library/react';
 import Header from './Header';
 
 describe('Header component', () => {
-  it('does render the Header', async () => {
+  it('should render the Header', async () => {
     render(<Header />);
     const header = await screen.findByRole('banner', { name: 'header' });
     expect(header).toBeInTheDocument();
+    expect(header).toHaveClass('header');
   });
 });
