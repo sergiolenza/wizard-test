2bef75bd8e3ff1f5427cd92b171733f2452fe7ba
Sergio Lenza <slenzamori.dev@gmail.com>
2022-05-18 10:06:27 +0200

    test: add more tests to Button

--- src/components/Button/Button.test.js
+++ src/components/Button/Button.test.js
@@ -5,14 +5,37 @@ import Button from './Button';
 const mockClickFn = jest.fn();
 
 describe('Button component', () => {
-  it('does render the Button', async () => {
-    render(<Button>Add to cart</Button>);
-    const button = await screen.findByText('Add to cart');
+  it('should render the Button', async () => {
+    render(<Button>Next</Button>);
+    const button = await screen.findByRole('button');
     expect(button).toBeInTheDocument();
+    expect(button).toHaveClass('button');
   });
-  it('does call the onClick function', async () => {
-    render(<Button onClick={mockClickFn}>Add to cart</Button>);
-    const button = await screen.findByText('Add to cart');
+  it('should have the contained variant class', async () => {
+    render(<Button variant="contained">Next</Button>);
+    const button = await screen.findByRole('button');
+    expect(button).toHaveClass('contained');
+    expect(button).not.toHaveClass('text');
+  });
+  it('should have the text variant class', async () => {
+    render(<Button variant="text">Next</Button>);
+    const button = await screen.findByRole('button');
+    expect(button).toHaveClass('text');
+    expect(button).not.toHaveClass('contained');
+  });
+  it('should forward the rest of the props', async () => {
+    render(
+      <Button name="test-button" disabled>
+        Next
+      </Button>
+    );
+    const button = await screen.findByRole('button');
+    expect(button).toHaveAttribute('name');
+    expect(button).toHaveAttribute('disabled');
+  });
+  it('should call the onClick function when clicked', async () => {
+    render(<Button onClick={mockClickFn}>Next</Button>);
+    const button = await screen.findByRole('button');
     await userEvent.click(button);
     expect(mockClickFn).toHaveBeenCalled();
   });
