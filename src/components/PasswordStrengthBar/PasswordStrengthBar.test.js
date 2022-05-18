48b1092dbc507d870542ab4a3c776cae31b8d0b9
Sergio Lenza <slenzamori.dev@gmail.com>
2022-05-18 10:54:32 +0200

    test: add more test to PasswordStrengthBar

--- src/components/PasswordStrengthBar/PasswordStrengthBar.test.js
+++ src/components/PasswordStrengthBar/PasswordStrengthBar.test.js
@@ -1,18 +1,62 @@
 import { render, screen } from '@testing-library/react';
 import PasswordStrengthBar from './PasswordStrengthBar';
 
-const renderPasswordStrengthBar = () => render(<PasswordStrengthBar strength={0} />);
-
 describe('PasswordStrengthBar component', () => {
-  it('does render the PasswordStrengthBar', async () => {
-    renderPasswordStrengthBar();
+  it('should render the PasswordStrengthBar', async () => {
+    render(<PasswordStrengthBar strength={0} />);
     const passwordStrengthBar = await screen.findByRole('figure', {
       name: 'password-strength-bar',
     });
     expect(passwordStrengthBar).toBeInTheDocument();
   });
-  it('does have the proper class', async () => {
-    const { container } = renderPasswordStrengthBar();
-    expect(container.firstChild.classList.contains('too-weak')).toBe(true);
+  it('should have the too-weak class by default', async () => {
+    render(<PasswordStrengthBar />);
+    const passwordStrengthBar = await screen.findByRole('figure', {
+      name: 'password-strength-bar',
+    });
+    expect(passwordStrengthBar).toHaveClass('too-weak');
+    expect(passwordStrengthBar).not.toHaveClass('weak');
+    expect(passwordStrengthBar).not.toHaveClass('medium');
+    expect(passwordStrengthBar).not.toHaveClass('strong');
+  });
+  it('should have the too-weak class', async () => {
+    render(<PasswordStrengthBar strength={0} />);
+    const passwordStrengthBar = await screen.findByRole('figure', {
+      name: 'password-strength-bar',
+    });
+    expect(passwordStrengthBar).toHaveClass('too-weak');
+    expect(passwordStrengthBar).not.toHaveClass('weak');
+    expect(passwordStrengthBar).not.toHaveClass('medium');
+    expect(passwordStrengthBar).not.toHaveClass('strong');
+  });
+  it('should have the weak class', async () => {
+    render(<PasswordStrengthBar strength={1} />);
+    const passwordStrengthBar = await screen.findByRole('figure', {
+      name: 'password-strength-bar',
+    });
+    expect(passwordStrengthBar).not.toHaveClass('too-weak');
+    expect(passwordStrengthBar).toHaveClass('weak');
+    expect(passwordStrengthBar).not.toHaveClass('medium');
+    expect(passwordStrengthBar).not.toHaveClass('strong');
+  });
+  it('should have the medium class', async () => {
+    render(<PasswordStrengthBar strength={2} />);
+    const passwordStrengthBar = await screen.findByRole('figure', {
+      name: 'password-strength-bar',
+    });
+    expect(passwordStrengthBar).not.toHaveClass('too-weak');
+    expect(passwordStrengthBar).not.toHaveClass('weak');
+    expect(passwordStrengthBar).toHaveClass('medium');
+    expect(passwordStrengthBar).not.toHaveClass('strong');
+  });
+  it('should have the strong class', async () => {
+    render(<PasswordStrengthBar strength={3} />);
+    const passwordStrengthBar = await screen.findByRole('figure', {
+      name: 'password-strength-bar',
+    });
+    expect(passwordStrengthBar).not.toHaveClass('too-weak');
+    expect(passwordStrengthBar).not.toHaveClass('weak');
+    expect(passwordStrengthBar).not.toHaveClass('medium');
+    expect(passwordStrengthBar).toHaveClass('strong');
   });
 });
